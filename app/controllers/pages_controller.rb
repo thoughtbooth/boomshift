777aa8a3f70da1d7ec7e653d256c44e4ce23c08a
Tim Elton <tim.elton@thoughtbooth.com>
2015-02-25 04:36:47 +0000

    Created main pages of site, added site navigation, setup Bootstrap, and setup Devise for user authentication

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def business
+  end
+  
+  def clients
+  end
+  
+  def schedule
+  end
+  
+  def advertising
+  end
+  
+  def reports
+  end
+end
