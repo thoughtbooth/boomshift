b370f080ab1c7c0782f0307a6ec1593df637b52b
Tim Elton <tim.elton@thoughtbooth.com>
2015-08-26 04:18:16 +0000

    Fixed timezone bug by removing gem.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.cookie
-//= require jstz
-//= require browser_timezone_rails/set_time_zone
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
