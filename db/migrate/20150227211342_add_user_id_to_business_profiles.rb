c2193c306d93faf9681e03bd36d96c11651c985b
Tim Elton <tim.elton@thoughtbooth.com>
2015-12-28 22:26:55 +0000

    Deleted bad migrations for business_profile and client_profile.

--- db/migrate/20150227211342_add_user_id_to_business_profiles.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToBusinessProfiles < ActiveRecord::Migration
-  def change
-    add_column :business_profiles, :user_id, :integer
-    add_index :business_profiles, :user_id
-  end
-end
--- db/migrate/20150227211400_add_user_id_to_client_profiles.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToClientProfiles < ActiveRecord::Migration
-  def change
-    add_column :client_profiles, :user_id, :integer
-    add_index :client_profiles, :user_id
-  end
-end
