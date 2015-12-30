f48c357ff680908757afbe0487336df5f0aa3831
Tim Elton <tim.elton@thoughtbooth.com>
2015-12-30 23:28:35 +0000

    Fixed bootstrap tooltip issue by installing jquery_turbolinks gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
 //= require pickers
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip()
+});
