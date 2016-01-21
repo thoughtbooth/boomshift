bce9dee94b75b5cae321d788e12ca308cfb7e816
Tim Elton <tim.elton@thoughtbooth.com>
2016-01-21 17:22:37 +0000

    Trying to fix figaro deployment issue.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,10 +43,10 @@ Rails.application.configure do
   
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT_DEV'],
-    :address        => ENV['MAILGUN_SMTP_SERVER_DEV'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN_DEV'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD_DEV'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'boomshift.heroku.com',
     :authentication => :plain
     #:enable_starttls_auto => true
